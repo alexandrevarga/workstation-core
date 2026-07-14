c8c467024d07468ff463aa2d952fc5264c352660
alexandrevarga <52091024+alexandrevarga@users.noreply.github.com>
2026-07-13 22:57:57 -0300

    feat: add foot and alacritty to bootstrap orchestration

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,4 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 sudo dnf upgrade -y
-sudo dnf install -y make stow git
+sudo dnf install -y make stow git foot alacritty
